185cb9ad8349e9680b6830c5280f3764f85c5f33
ahmedherhesh <ahmedherhesh3@gmail.com>
2021-11-01 05:23:23 +0200

    make resource

--- App/Http/Resources/UserResources.php
+++ App/Http/Resources/UserResources.php
@@ -8,15 +8,15 @@ class UserResources
     public function __construct($data)
     {
         $data = [
-            "id" => $data->id,
-            "type" => $data->type,
-            "username" => $data->username,
-            "mobile" => $data->mobile,
-            "email" => $data->email,
-            "package_id" => $data->package_id,
-            "city_id" => $data->city_id,
-            "banned" => $data->banned,
-            "email_verified_at" => $data->email_verified_at,
+            'id' => $data->id,
+            'type' => $data->type,
+            'username' => $data->username,
+            'mobile' => $data->mobile,
+            'email' => $data->email,
+            'package_id' => $data->package_id,
+            'city_id' => $data->city_id,
+            'banned' => $data->banned,
+            'email_verified_at' => $data->email_verified_at,
         ];
         $this->data = $data;
     }
